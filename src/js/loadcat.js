a9dab863cfd96bcf62732f97e8d11b9d79b71b60
arccosine <arc@tunsns.net>
2018-09-29 15:42:24 +0900

    [Update] Change Cat get API.Flickr to the cat api.

--- src/js/loadcat.js
+++ src/js/loadcat.js
@@ -9,18 +9,16 @@ const loadCat = (source)=>{
     });
 }
 
-window.nyaan = (res)=>{
-    const pos = Math.floor(Math.random()*res.length);
-    const catLink = res[pos];
-    loadCat(catLink).then((img)=>{
-        document.querySelector('header img').src = img;
-    });
-};
-
 // async load.
 const loadScript = (()=>{
-    const script = document.createElement('script');
-    script.src = 'https://script.google.com/macros/s/AKfycbzeDnzoGh0ht17HfPlrCRjoVouWxYvpYP3aPafox2FzAfnxnc79/exec?callback=nyaan';
-    script.async = true;
-    document.body.appendChild(script);
+    fetch('https://api.thecatapi.com/v1/images/search?size=small&mime_type=jpg,png&format=json&order=RANDOM&limit=10&8ade48b6-7830-43aa-9c6b-5c557d072995').then((response)=>{
+        return response.json()
+    }).then((jsonres)=>{
+        const pos = Math.floor(Math.random()*jsonres.length);
+        const catLink = jsonres[pos];
+        console.log(catLink.url);
+        loadCat(catLink.url).then((img)=>{
+            document.querySelector('header img').src = img;
+        });
+    });
 })();
